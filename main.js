6284ac05de7741523c11489e9109ffb7de0ba502
Joel Rosas <joeli.rosas20@gmail.com>
2023-02-16 21:04:20 -0500

    Added a modal to my site

---
+++ main.js
@@ -0,0 +1,23 @@
+//Get the modal
+var modal = document.getElementById("filterModal");
+//Get the button that closes the modal
+var btn = document.getElementById("filterBtn");
+//Get the span element the closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//Once the user clicks the button, the modal appears
+btn.onclick = () => {
+    modal.style.display = "block"
+}
+
+//For the close button on the modal
+span.onclick = () => {
+    modal.style.display = "none"
+}
+
+//For the modal to close when the user clicks anywhere on the screen
+window.onclick = (e) =>{
+    if(e.target == modal){
+        modal.style.display = "none"
+    }
+}
