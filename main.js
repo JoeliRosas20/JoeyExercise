5ba2e8eb357004ef82d7efbc0d793a95e33061fd
Joel Rosas <joeli.rosas20@gmail.com>
2023-02-27 18:31:31 -0500

    We can add exercises with its detail in the array

--- main.js
+++ main.js
@@ -31,6 +31,9 @@ let repsInput = document.getElementById("repsInput");
 let poundsInput = document.getElementById("poundsInput");
 let msg = document.getElementById("msg")
 let msg2 = document.getElementById("msg2")
+let msg3 = document.getElementById("msg3")
+let msg4 = document.getElementById("msg4")
+let add = document.getElementById("add")
 let data = [];
 
 form.addEventListener("submit", (e) =>{
@@ -40,6 +43,10 @@ form.addEventListener("submit", (e) =>{
 })
 
 let formValidation = () => {
+    let set = setsInput.value;
+    let rep = repsInput.value;
+    let pound = poundsInput.value;
+    //Name validatio
     if(textInput.value === ""){
         console.log("formValidation called Failure")
         msg.innerHTML = "It cannot be blank"
@@ -47,19 +54,54 @@ let formValidation = () => {
         console.log("formValidation called Success")
         msg.innerHTML = ""
     }
-    if(setsInput.value < 0){
+    //Sets validation
+    if(set < 0){
         msg2.innerHTML = "It cannot be negative"
     }
-    if(isNaN(setsInput.value)){
+    else if(isNaN(set)){
         msg2.innerHTML = "It needs to be a number"
+    }else if(set === ""){
+        msg2.innerHTML = "It cannot be blank"
     }else{//This will clear the warning
         msg2.innerHTML = ""
+    }
+    //Reps validation
+    if(rep < 0){
+        msg3.innerHTML = "It cannot be negative"
+    }else if(isNaN(rep)){
+        msg3.innerHTML = "It needs to be a number"
+    }else if(rep === ""){
+        msg3.innerHTML = "It cannot be blank"
+    }else{
+        msg3.innerHTML = ""
+    }
+    //Pounds validation
+    if(pound < 0){
+        msg4.innerHTML = "It cannnot be negative"
+    }else if(isNaN(pound)){
+        msg4.innerHTML = "It needs to be a number"
+    }else if(pound === ""){
+        msg4.innerHTML = "It cannot be blank"
+    }else{
+        msg4.innerHTML = ""
+    }
+
+    if(textInput.value !== "" && (set !== "" && !isNaN(set)) && (rep !== "" && !isNaN(rep)) && (pound !== "" && !isNaN(pound))){
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal")//This changes the html to dismiss
+        add.click();//This clicks the dismiss button
+        (() => {
+            add.setAttribute("data-bs-dismiss","")
+        })();//This makes sure that the form does not get send with blank inputs
     }
 }
 
 let acceptData = () =>{
     data.push({
-        text: textInput.value,
+        name: textInput.value,
+        set: setsInput.value,
+        rep: repsInput.value,
+        pound: poundsInput.value,
     })
     localStorage.setItem("data", JSON.stringify(data))
     console.log(data)
